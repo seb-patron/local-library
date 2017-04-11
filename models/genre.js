44dfbf694e05512e4771b7658629bcb7c6a9f3df
Sebastian Patron <sebastian.patrones@gmail.com>
2017-04-10 21:30:32 -0700

    book details implemented

--- models/genre.js
+++ models/genre.js
@@ -2,16 +2,16 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var GenreInstance = Schema({
+var GenreSchema = Schema({
   name: { type: String, required: true, min: 3, max: 100 }, //reference to the associated book
 });
 
 // Virtual for bookinstance's URL
-GenreInstance
+GenreSchema
 .virtual('url')
 .get(function () {
   return '/catalog/genre/' + this._id;
 });
 
 //Export model
-module.exports = mongoose.model('GenreInstance', GenreInstance);
+module.exports = mongoose.model('Genre', GenreSchema);
